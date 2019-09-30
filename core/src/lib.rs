fa2bf989f350385d01469edcd99b1e26c3939d36
Michael Bryan <michaelfbryan@gmail.com>
2019-09-30 18:00:18 +0800

    Fleshed out the core crate

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,7 +1,41 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub static CORE_VERSION: &str = env!("CARGO_PKG_VERSION");
+pub static RUSTC_VERSION: &str = env!("RUSTC_VERSION");
+
+pub trait Function {
+    fn call(&self, args: &[f64]) -> Result<f64, InvocationError<'_>>;
+
+    /// Help text that may be used to display information about this function.
+    fn help(&self) -> Option<&str> {
+        None
     }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum InvocationError<'a> {
+    InvalidArgumentCount { expected: usize, found: usize },
+    Other { msg: &'a str },
+}
+
+#[derive(Copy, Clone)]
+pub struct PluginDeclaration {
+    pub rustc_version: &'static str,
+    pub core_version: &'static str,
+    pub register: fn(&mut dyn PluginRegistrar),
+}
+
+pub trait PluginRegistrar {
+    fn register_func(&mut self, name: &str, function: Box<dyn Function>);
+}
+
+#[macro_export]
+macro_rules! export_plugin {
+    ($register:expr) => {
+        #[doc(hidden)]
+        #[no_mangle]
+        pub static plugin_declaration: $crate::PluginDeclaration = $crate::PluginDeclaration {
+            rustc_version: $crate::RUSTC_VERSION,
+            core_version: $crate::CORE_VERSION,
+            register: $register,
+        };
+    };
 }
